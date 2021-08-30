d546eeea1e0614cdeb8838af23d84ac959ba27fc
Amir Bakhshi <amirmobili7@gmail.com>
2021-08-30 21:13:22 +0200

    Delegate pattern, ui updating based on request, some refactorinng

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -15,7 +15,7 @@ protocol CoinManagerDelegate  {
 
 struct CoinManager {
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "03E6A695-8850-4F49-B9BF-5D1C532F1AB1"
+    let apiKey = "x"
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
     
     var delegate: CoinManagerDelegate?
