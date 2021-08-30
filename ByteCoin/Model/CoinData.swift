27a3eef8908e5e45828ecbd4b172d5d46b962a74
Amir Bakhshi <amirmobili7@gmail.com>
2021-08-30 19:06:07 +0200

    URLSession, parseJSON method implemented

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,12 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Amir Bakhshi on 2021-08-30.
+//  Copyright © 2021 Baam. All rights reserved.
+//
+
+import Foundation
+struct CoinData: Codable {
+    let rate: Double
+}
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -9,11 +9,42 @@
 import Foundation
 
 struct CoinManager {
-    
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
-    
+    let apiKey = "94285B73-0451-4882-BFE6-C16A69AB6950"
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
     
+    func getCoinPrice(currency: String) {
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    if let rate = self.parseJSON(safeData) {
+                        let roundedRate = String(format: "%.2f", rate)
+                        print(roundedRate)
+                    }
+                }
+            }
+            
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ data: Data) -> Double? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            let lastPrice = decodedData.rate
+            return lastPrice
+            
+        } catch {
+            return nil
+        }
+    }
 }
